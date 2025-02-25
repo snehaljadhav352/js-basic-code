e6e3fb19573c91ae623cf91c18e5f6e8d46d5350
Snehal Jadhav <78867821+snehaljadhav352@users.noreply.github.com>
2025-02-25 19:52:49 +0000

    stack and heap memory

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -11,13 +11,12 @@ let isLoggedIn = true;
 let accountId = Symbol("123")
 let id = Symbol("123")
 
-console.log(accountId===id)  // false
-
-console.log(typeof score);  //number
-console.log(typeof userName); //string
-console.log(typeof outsideTemp); // object
-console.log(typeof isLoggedIn); // boolean
-console.log(typeof accountId);  //symbol
+// console.log(accountId===id)  // false
+// console.log(typeof score);  //number
+// console.log(typeof userName); //string
+// console.log(typeof outsideTemp); // object
+// console.log(typeof isLoggedIn); // boolean
+// console.log(typeof accountId);  //symbol
 
 
 // Non Primitive (Refrence Datatypes)
@@ -34,6 +33,45 @@ const user = {
 const newFunction = function(){
     console.log("Hello")
 }
-console.log(typeof cities); // object
-console.log(typeof user);  // object
-console.log(typeof newFunction); // function
+// console.log(typeof cities); // object
+// console.log(typeof user);  // object
+// console.log(typeof newFunction); // function
+
+
+
+
+/*****************************************************************************************************************************/
+
+/* 
+In Javascript there are two memories
+Stack=> used in primitive datatypes
+       in stack creates a copy of variable
+Heap=> used in non-primitive datatypes
+      in heap whatever change happen it will change or update in original place or refrence
+*/
+
+const accountName = "Raviraj Singh"
+let accountNameTwo = accountName;
+accountNameTwo = "Snehal jadhav"
+
+console.log(accountName)  // "Raviraj Singh"
+console.log(accountNameTwo)  //"Snehal jadhav"
+/* so here js creates a copy of variable and when we change or update the value
+ it will make changes in copy variable not is actual variable */
+
+
+ const userOne = {
+    email:"user@gmail.com",
+    upi:"user@ybl"
+ }
+
+ const userTwo = userOne
+
+ userTwo.email = "john@gmail.com"
+
+ console.log(userOne)
+ console.log(userTwo)
+
+ /* userTwo and userOne are stored in stack but the values are stored in heap memory
+ here in objects store in heap memory and whenever we change or update 
+ it will refer to the actual object it does not make a copy */
