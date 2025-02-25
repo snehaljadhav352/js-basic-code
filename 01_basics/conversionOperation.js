5d93b21dd6a2f40342f788b4c89c6fcf385e6394
Snehal Jadhav <78867821+snehaljadhav352@users.noreply.github.com>
2025-02-25 12:28:52 +0000

    conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,32 @@
+ let score = false;
+
+//  console.log(typeof score)  // string
+//  console.log(typeof (score)) // string
+
+ // conversion to Number
+ let convertToNumber = Number(score)
+//  console.log(typeof convertToNumber)
+//  console.log(convertToNumber)
+
+ // "33" => 33 (type=number)
+ // "33abc" => NaN (type=number)
+ // "null" => 0 (type=number)
+ // true/false => 1/0 (type=number)
+
+ // Conversion to Boolean
+ let age = null;
+ let convertToBolean = Boolean(age)
+//  console.log(typeof convertToBolean)
+//  console.log(convertToBolean)
+
+ // ""=> false
+ // "snehal"=>true
+ // 33 => true
+ //null => false
+
+
+ // conversion to String
+ let someNumber = true
+let converToString = String(someNumber)
+console.log(converToString)
+console.log(typeof converToString)
