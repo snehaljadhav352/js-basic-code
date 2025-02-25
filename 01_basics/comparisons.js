7b565cd2b3dc5cc182caaffd06a8a0088ad994df
Snehal Jadhav <78867821+snehaljadhav352@users.noreply.github.com>
2025-02-25 19:26:09 +0000

    comparisons

---
+++ 01_basics/comparisons.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2==1);
+// console.log(2<=1);
+// console.log(2!==1);
+
+
+// console.log("2">1)
+// console.log("02">1)
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0) // better to avoid these type of comparisons "language inconsistency"
+/*
+The reason that equality check "==" and comparisons ">,<,<=,>=" work differently.
+Comparisons convert null to a number, treating as 0
+Thats why "null>=0" is true and "null>0" is flase.
+*/
+
+
+
